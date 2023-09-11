3a7cf71fc24bd23ac2c2baf5341c9b7e1e43ed2f
Vincent Garcia <vincentfpgarcia@gmail.com>
2023-09-11 22:11:41 +0200

    feat: working version

--- mobilenet-v3-app/ContentView.swift
+++ mobilenet-v3-app/ContentView.swift
@@ -7,17 +7,139 @@
 
 import SwiftUI
 
+fileprivate let radius = CGFloat(15)
+
 struct ContentView: View {
+
+    @StateObject private var engine = Engine()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+        ZStack {
+
+            BackgroundView(engine: engine)
+
+            VStack(spacing: 20) {
+                ImageView(engine: engine)
+                InfoView(engine: engine)
+            }
+            .padding()
         }
-        .padding()
+        .task {
+            await engine.camera.start()
+        }
     }
 }
+
+
+struct BackgroundView: View {
+
+    @ObservedObject var engine: Engine
+
+    var body: some View {
+
+
+
+        GeometryReader { geo in
+
+
+//            let proba = $engine.proba
+//            let (red, green, blue) = computeColor()
+//            computeColor()
+//            Color(red: red, green: green, blue: blue)
+//            Color(UIColor.lightGray)
+
+            Color(white: 0.95)
+
+        }
+        .ignoresSafeArea()
+    }
+
+    func computeColor() -> Color {
+//        guard let proba = engine.proba else {
+//            return (0.8, 0.8, 0.8)
+//        }
+        guard let probability = engine.probability else {
+            return Color(red: 0.8, green: 0.8, blue: 0.8)
+        }
+
+        if probability < 0.2 {
+            return Color(red: 1.0, green: 0.8, blue: 0.8)
+        }
+
+        if probability < 0.4 {
+            return Color(red: 1.0, green: 1.0, blue: 0.8)
+        }
+
+        return Color(red: 0.8, green: 1.0, blue: 0.8)
+    }
+}
+
+
+struct ImageView: View {
+
+    @ObservedObject var engine: Engine
+
+    var body: some View {
+        if let image = engine.viewfinderImage {
+            image
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(radius)
+        }
+    }
+}
+
+
+//struct TextInfo: ViewModifier {
+//    func body(content: Content) -> some View {
+//        content
+////            .font(.headline)
+//    }
+//}
+//
+//extension View {
+//    func textStyle() -> some View {
+//        modifier(TextInfo())
+//    }
+//}
+
+
+struct InfoView: View {
+
+    @ObservedObject var engine: Engine
+
+    var body: some View {
+
+        VStack(spacing: 10) {
+
+            let label = engine.label != nil ? engine.label! : "Undefined"
+            HStack {
+                Text("What")
+                Spacer()
+                Text(label)
+            }
+
+            let proba = engine.probability != nil ? "\(Int(engine.probability! * 100)) %" : "0 %"
+            HStack {
+                Text("Confidence")
+                Spacer()
+                Text(proba)
+            }
+
+            let fps = engine.fps != nil ? engine.fps! : 0
+            HStack {
+                Text("Inference per second")
+                Spacer()
+                Text("\(fps)")
+            }
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(radius)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ mobilenet-v3-app/Engine.swift
@@ -0,0 +1,124 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import AVFoundation
+import SwiftUI
+import os.log
+import MobileNetV3Package
+
+final class Engine: ObservableObject {
+    let camera = Camera()
+
+    @Published var viewfinderImage: Image?
+    @Published var cgImage: CGImage?
+    @Published var debugImage: Image?
+    @Published var label: String?
+    @Published var probability: Double?
+    @Published var fps: Int?
+
+    init() {
+        Task {
+            await handleCameraPreviews()
+        }
+        Task {
+            await processPicture()
+        }
+    }
+
+    func handleCameraPreviews() async {
+//        let imageStream = camera.previewStream
+//            .map { $0.image }
+//
+//        for await image in imageStream {
+//            Task { @MainActor in
+//                viewfinderImage = image
+//            }
+//        }
+        for await image in camera.previewStream {
+            Task { @MainActor in
+                cgImage = image.cgImage
+                viewfinderImage = image.image
+            }
+        }
+    }
+
+
+    func processPicture() async {
+
+        let model = MobileNetV3Package()!
+
+        while true {
+
+            if let image = cgImage {
+
+                print("Image size: \(image.width) x \(image.height)")
+
+//                // Crop and resize the image
+//                let w = image.width
+//                let h = image.height
+//                let l = min(w, h)
+//                let x = (w - l) / 2
+//                let y = (h - l) / 2
+//                let imageCropped = image.cropping(to: CGRect(x: x, y: y, width: l, height: l))!
+//                let imageResized = imageCropped.resize(size: CGSize(width: 224, height: 224))!
+
+                // Resize the image
+                let imageResized = image.resize(size: CGSize(width: 224, height: 224))!
+
+                // Perform the inference
+                let start = DispatchTime.now()
+                let (label, probability) = model.predict(input: UIImage(cgImage: imageResized))
+                let end = DispatchTime.now()
+
+                // Compute the average inference time
+                let durationInMs = Double(end.uptimeNanoseconds - start.uptimeNanoseconds) / 1000000.0
+                let fps = Int(1000.0 / durationInMs)
+
+                // Update the UI
+                Task { @MainActor in
+                    self.debugImage = Image(decorative: imageResized, scale: 1, orientation: .up)
+                    self.label = label
+                    self.probability = probability
+                    self.fps = fps
+                }
+
+            }
+
+//            sleep(1)
+        }
+    }
+
+}
+
+fileprivate extension CIImage {
+    var image: Image? {
+        let ciContext = CIContext()
+        guard let cgImage = ciContext.createCGImage(self, from: self.extent) else { return nil }
+        return Image(decorative: cgImage, scale: 1, orientation: .up)
+    }
+
+    var cgImage: CGImage? {
+        let ciContext = CIContext()
+        guard let cgImage = ciContext.createCGImage(self, from: self.extent) else { return nil }
+        return cgImage
+    }
+}
+
+fileprivate extension CGImage {
+    func resize(size:CGSize) -> CGImage? {
+        let width: Int = Int(size.width)
+        let height: Int = Int(size.height)
+
+        let bytesPerPixel = self.bitsPerPixel / self.bitsPerComponent
+        let destBytesPerRow = width * bytesPerPixel
+
+        guard let colorSpace = self.colorSpace else { return nil }
+        guard let context = CGContext(data: nil, width: width, height: height, bitsPerComponent: self.bitsPerComponent, bytesPerRow: destBytesPerRow, space: colorSpace, bitmapInfo: self.alphaInfo.rawValue) else { return nil }
+
+        context.interpolationQuality = .high
+        context.draw(self, in: CGRect(x: 0, y: 0, width: width, height: height))
+
+        return context.makeImage()
+    }
+}

f17d3240d3dfe6158dc0dfd5a51739212142b9cd
Vincent Garcia <vincentfpgarcia@gmail.com>
2023-09-11 09:41:03 +0200

    feat: add hello world app

---
+++ mobilenet-v3-app/mobilenet_v3_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobilenet_v3_appApp.swift
+//  mobilenet-v3-app
+//
+//  Created by Vincent Garcia on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct mobilenet_v3_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
